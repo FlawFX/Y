29882e526a7ead5fd3ab047ab4063fb9243b83b8
DaniBotz <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-09-18 06:21:23 +0800

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -5,11 +5,11 @@ let fetch = require('node-fetch')
          let gc1 = '34649537982-1370248067@g.us' 
          let gc2 = '628112958665-1628163967@g.us' 
          let gc3 = '628112958665-1571053173@g.us' 
-         let _gc1 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc1) 
-         let _gc2 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc2) 
-         let _gc3 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc3) 
+         let _gc1 = 'https://chat.whatsapp.com/KiKVjpgL4EH6i0kMUtlEJz' + await conn.groupInviteCode(gc1) 
+         let _gc2 = 'https://chat.whatsapp.com/KiKVjpgL4EH6i0kMUtlEJz' + await conn.groupInviteCode(gc2) 
+         let _gc3 = 'https://chat.whatsapp.com/KiKVjpgL4EH6i0kMUtlEJz' + await conn.groupInviteCode(gc3) 
          let caption = ` 
- *Join juga group official Cute Bot!* 
+ *Join juga group official Dani Bot!* 
  Sekedar meramaikan, atau juga jika ada informasi mengenai bot. 
   
  *Group 1* 
@@ -24,28 +24,28 @@ let fetch = require('node-fetch')
  `.trim() 
          await conn.reply(m.chat, caption, m, { contextInfo: { 
              externalAdReply: { 
-               sourceUrl: 'https://youtu.be/RlhkqeUDZrA', 
+               sourceUrl: 'https://youtu.be/Uc6-ReBLX9k', 
                title: 'Official group', 
-               body: 'Raa Bot', 
+               body: 'Dani Bot', 
                thumbnail: pp 
              } 
          }}) 
      } catch (e) { 
          let tulisan = ` 
- *Claraa Bot!* 
+ *Dani Bot!* 
 Sekedar meramaikan, atau juga jika ada informasi mengenai bot. 
   
  *Group 1* 
-https://chat.whatsapp.com/Gapunya
+https://chat.whatsapp.com/KiKVjpgL4EH6i0kMUtlEJz
  
  `.trim() 
      await conn.sendButtonDoc(m.chat, tulisan.trim(), 'Harap patuhi rules dan peraturan pada masing masing group ya! Terimakasih.', 'Rules', '.rules', m, {
     quoted: m,
     contextInfo: { forwardingScore: 99999, isForwarded: true,
         externalAdReply: {
-        	sourceUrl: 'https://instagram.com/inizenscuy/',
+        	sourceUrl: 'https://instagram.com/danibotz_official',
             title: 'Gruop Official',
-            body: '© RaaBotz-MD By Claraa San',
+            body: '© DaniBotz-MD By Dani Official',
           thumbnail: await (await fetch('https://telegra.ph/file/f05029f17de62fb420459.jpg')).buffer()
         }
      }
