641938fee59386b78e55a409864311efaa7bd414
FlawFX <114559769+FlawFX@users.noreply.github.com>
2022-09-30 07:10:56 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Telkom [081251549899]
-│ • Gopay  [081352848936]
-│ • Dana  [081251549899]
+│ • Telkom [089510598381]
+│ • Gopay  [089510598381]
+│ • Dana  [089510598381]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6281251549899
+│ > Ingin donasi? Wa.me/6289510598381
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
